47830dc8ab452871c81b87828a61d5a47e5d1722
Dennis Griffin <dgdev1024@gmail.com>
2022-03-17 23:40:37 -0400

    Fixed an error in the Html tag in _document.js

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&family=Ubuntu+Mono&display=swap"
